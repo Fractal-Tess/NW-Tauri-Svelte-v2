10b10f85db0a4c1e43b6d0c5e310d115490c3b82
Fractal-Tess <vgfractal@gmail.com>
2022-06-28 14:14:09 +0300

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,6 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<audio src="testing/audio.aac" controls></audio>
+<video src="testing/video.mp4" controls></video>
+<img src="testing/image.jpg" alt="">
